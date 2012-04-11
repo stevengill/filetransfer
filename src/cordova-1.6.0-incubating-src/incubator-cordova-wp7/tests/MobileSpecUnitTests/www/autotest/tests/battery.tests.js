a9f6e7f86454d7a3076fb707554373f2b4af29e0
Steven Gill <stevengill97@gmail.com>
2012-04-11 10:26:19 -0700

    updated files

--- src/cordova-1.6.0-incubating-src/incubator-cordova-wp7/tests/MobileSpecUnitTests/www/autotest/tests/battery.tests.js
+++
@@ -1,7 +0,0 @@
-Tests.prototype.BatteryTests = function() {  
-    module('Battery (navigator.battery)');
-    test("should exist", function() {
-        expect(1);
-        ok(navigator.battery != null, "navigator.battery should not be null.");
-    });
-};
